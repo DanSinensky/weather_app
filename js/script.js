2af3e867f45bd15734ac4dafa8e928d4ec90b3ce
Dan Sinensky <dan.i.sinensky@gmail.com>
2022-10-22 17:04:44 -0400

    Comments

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const apiKey = "d05a2546f3bf66fa6eb232d95995a8d2"
 
 // variable for base url
 const baseURL = "http://api.openweathermap.org/"
-//http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d05a2546f3bf66fa6eb232d95995a8d2
 
 // function that does movie search
 function weatherSearch(city){
     // constructing url for request
     const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + apiKey
+    // Hardcoded url for London and my api
     // http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d05a2546f3bf66fa6eb232d95995a8d2
     console.log(url)
     
@@ -26,19 +26,18 @@ function weatherSearch(city){
         function render() {
 
         // render the data
-        const $subheader = $(".header-container")
         const $left = $(".left")
         const $main = $("main")
         const $right = $(".right")
-        const $footer = $("footer")
-
-        // const areas = [$left, $main, $right, $footer]
-        // areas.forEach(function(item,index){item.empty})
 
         $main.html(
             `<h2>City: ${weather.name}</h2>
             <h3>Current Weather: ${weather.weather[0].description}`)
-        $left.html(`<h3>Temperature: ${Math.floor(((weather.main.temp-273.15)*9/5)+32)} °F</br>Feels Like: ${Math.floor(((weather.main.feels_like-273.15)*9/5)+32)} °F</h3>`)
+            // Put both Temperature and Feels Like in one tag with a line break
+            //so the second doesn't overwrite the first). Convert to Fahrenheit
+        $left.html(`<h3>Temperature: ${Math.floor(((weather.main.temp-273.15)*9/5)+32)} °F
+        </br>Feels Like: ${Math.floor(((weather.main.feels_like-273.15)*9/5)+32)} °F</h3>`)
+        // Get img based on json info for icon
         $right.html(`<img src="http://openweathermap.org/img/wn/${weather.weather[0].icon}.png">`)
     }
 
@@ -49,12 +48,12 @@ $("input[type=submit]").on("click", (event) => {
 
     // prevent the refresh
     event.preventDefault()
-
     // grab text from input box
     const $inputText = $("input[type=text]").val()
     const $textInput = $("input[type=text]")
     // update the screen
     weatherSearch($inputText)
+    // Remove text from input after updating info
     $textInput.val("")
 })
 
